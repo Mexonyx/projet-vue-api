435343e1a3cbee8cc68d4ebe62fe47e9d87d62cd
MAXIME LEDRU <maxime.ledru4@orange.fr>
2023-01-11 08:39:10 +0100

    ca fonctionne

---
+++ src/components/AStation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="div">
+    <b style="font-size: 25px">{{ nom }}</b
+    ><br />
+    <i style="font-size: 15px">{{ adresse }} {{ cp }}</i
+    ><br />
+    Conditions : {{ conditions }}<br />
+    Exloitant : {{ exploitant }}<br />
+    Horaires d'ouverture : {{ ouverture }}<br />
+    Capacite de la station : {{ capacite }}<br />
+    <b v-if="ligne_tc" style="font-size: 17px"
+      ><i>Lignes proche de la station: {{ ligneTc }}</i></b
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AStation",
+  props: {
+    nom: String,
+    adresse: String,
+    cp: String,
+    conditions: String,
+    exploitant: String,
+    ouverture: String,
+    capacite: String,
+    ligneTc: String,
+  },
+};
+</script>
+
+<style>
+.div {
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 14px;
+  display: table-cell;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,73 @@
 <template>
-  <div class="home">
-    <MyIndex />
+  <h1>Stations de vélo</h1>
+  <div v-if="stations">
+    <h1>Stations BiCloo Nantes</h1>
+    <label for="query"
+      >Rechercher par propriétés (adresse, nombre de place etc...)
+    </label>
+    <input type="text" :id="query" :value="query" :name="query" />
+    <ul style="list-style-type: none">
+      <li
+        v-for="station in stations"
+        v-bind:key="station.fields"
+        style="
+          display: inline-block;
+          padding: 14px;
+          align-content: space-between;
+        "
+      >
+        <AStation
+          :nom="station.fields.nom"
+          :adresse="station.fields.adresse"
+          :cp="station.fields.cp"
+          :conditions="station.fields.conditions"
+          :exploitant="station.fields.exploitant"
+          :ouverture="station.fields.ouverture"
+          :capacite="station.fields.capacite"
+          :ligneTc="station.fields.ligne_tc"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MyIndex from "@/components/MyIndex.vue";
+import AStation from "@/components/AStation.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
   components: {
-    MyIndex,
+    AStation,
+  },
+
+  data: function () {
+    return {
+      apiURL:
+        "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_stations-velos-libre-service-nantes-metropole&q=",
+      rowNumber: 10,
+      stations: null,
+      query: "",
+    };
+  },
+  created: function () {
+    this.fetchDataQueryAsync();
+  },
+
+  watch: {
+    query: "fetchDataQueryAsync",
+  },
+  methods: {
+    fetchDataQueryAsync: async function () {
+      try {
+        const response = await axios.get(this.apiURL + this.query);
+        console.log(response.data);
+        this.stations = response.data.records;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
